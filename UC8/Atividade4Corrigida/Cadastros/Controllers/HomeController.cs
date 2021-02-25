62e47cecaf43cf8087282625fe07abc4bb76bd1c
Kelly Kawase <kellykawase@gmail.com>
2021-02-25 02:53:24 -0300

    Tarefa 4
    Tarefa 4  da UC8

---
+++ UC8/Atividade4Corrigida/Cadastros/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Cadastros.Models;
+
+namespace Cadastros.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Home()
+        {
+            return View();
+        }
+                public IActionResult Sobre()
+        {
+            return View();
+        }
+                public IActionResult Produtos()
+        {
+            return View();
+        }
+                public IActionResult Portfolio()
+        {
+            return View();
+        }
+                public IActionResult Galeria()
+        {
+            return View();
+        }
+                public IActionResult Servicos()
+        {
+            return View();
+        }
+
+    }
+}
