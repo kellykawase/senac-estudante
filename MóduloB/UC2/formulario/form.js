0764cdf129998731855b94a5d9a21c47b57b1760
Kelly Kawase <kellykawase@gmail.com>
2021-04-21 01:06:14 -0300

    Updated

---
+++ MóduloB/UC2/formulario/form.js
@@ -0,0 +1,30 @@
+function desabilitar()
+{
+    var formElem = document.getElementById("formPrincipal");
+    for(var i = 0; i < formElem.elements.length; i++)
+    {
+        formElem.elements[i].disabled = true;
+    }
+}
+function habilitar()
+{
+    var formElem = document.getElementById("formPrincipal");
+    for(var i = 0; i < formElem.elements.length; i++)
+    {
+        formElem.elements[i].disabled = false;
+    }
+}
+function apagar()
+{
+    var formElem = document.getElementById("formPrincipal");
+    formElem.style.display = "none";
+}
+/*** Executa no carregamento da página */
+var listaP = document.getElementsByTagName("p");
+for(var i = 0; i< listaP.length; i++)
+{
+    listaP[i].style.fontSize = "18px";
+    listaP[i].style.fontFamily = "Helvetica, sans-serif";
+    listaP[i].style.margin = "20px 10px 20px 10px";
+}
+/****/
